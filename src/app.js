280146bbe6930b3b067290fda3a13533fd6115ca
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-23 18:00:51 +1200

    create function that receives the values from title and body inputs in the HTML / UI and sent this data to be added to the fake db.jason database when send this data by the post method and add it in to the database by the http API library

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get posts
 function getPosts() {
     http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+// Submit posts
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+
+    const data = {
+        title,
+        body
+    }
+
+    // Create post
+    http.post('http://localhost:3000/posts', data)
+    .then(data => {
+        getPosts();
+    })
     .catch(err => console.log(err));
 }
