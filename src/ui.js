50d93be3963f5c94259eda24adb45594f3e53f03
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-23 15:35:00 +1200

    create a method that loops throught the data, creates the needed html tags and insert it inside the main html fetching the UI with the posts

--- src/ui.js
+++ src/ui.js
@@ -8,7 +8,23 @@ class UI {
         this.forState = 'add';
     }
 
+    showPosts(posts){
+        let output = "";
+
+        posts.forEach( post => {
+            output += `
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h4 class="card-title">${post.title}</h4>
+                    <p class="card-text">${post.body}</p>
+                </div>
+            </div>
+            `
+        });
+        
+        this.post.innerHTML = output;
+    }
 
 }
 
-export const UI = new UI();
+export const ui = new UI();
