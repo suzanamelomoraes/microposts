debd8b935187a9234656901ff7e85d854ecb06d7
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-23 15:51:19 +1200

    call the UI method tha shows posts inside the function that get the posts

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import { http } from './http';
-import { UI } from './ui';
+import { ui } from './ui';
 
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts() {
     http.get('http://localhost:3000/posts')
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
 }
