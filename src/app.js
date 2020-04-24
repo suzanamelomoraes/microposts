74fa66d0f5dc775ab810fd59ef238d526511cb52
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-24 13:46:49 +1200

    Modify the function tha manages the posts submited to check if the user is adding a new post or submiting a edited one, as the add already has been written, include the if statement to call put http request when the user is updating the data, then send the data to the database, notify the user that the data has been updated and also return the state and UI to the initial state

--- src/app.js
+++ src/app.js
@@ -1,100 +1,117 @@
-import { http } from './http';
-import { ui } from './ui';
+import { http } from "./http";
+import { ui } from "./ui";
 
 // Get posts on DOM load
-document.addEventListener('DOMContentLoaded', getPosts);
+document.addEventListener("DOMContentLoaded", getPosts);
 
 // Listen for add post
-document.querySelector('.post-submit').addEventListener('click', submitPost);
+document.querySelector(".post-submit").addEventListener("click", submitPost);
 
 // Listen for delete
-document.querySelector('#posts').addEventListener('click', deletePost);
+document.querySelector("#posts").addEventListener("click", deletePost);
 
 // Listen for edit state
-document.querySelector('#posts').addEventListener('click', enableEdit);
+document.querySelector("#posts").addEventListener("click", enableEdit);
 
 // Listen for cancel edit state
-document.querySelector('.card-form').addEventListener('click', cancelEdit);
+document.querySelector(".card-form").addEventListener("click", cancelEdit);
 
 // Get posts
 function getPosts() {
-    http.get('http://localhost:3000/posts')
-    .then(data => ui.showPosts(data))
-    .catch(err => console.log(err));
+  http
+    .get("http://localhost:3000/posts")
+    .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err));
 }
 
 // Submit posts
 function submitPost() {
-    const title = document.querySelector('#title').value;
-    const body = document.querySelector('#body').value;
+  const title = document.querySelector("#title").value;
+  const body = document.querySelector("#body").value;
+  const id = document.querySelector("#id").value;
 
-    if(title === '' || body === ''){
-        ui.showAlert('Please fill in all fields', 'alert alert-danger');
-    } else {
-        const data = {
-            title,
-            body
-        }
-    
-        // Create post
-        http.post('http://localhost:3000/posts', data)
-        .then(data => {
-            ui.showAlert('Post added', 'alert alert-success');
-            ui.clearFields();
-            getPosts();
+  const data = {
+    title,
+    body,
+  };
+
+  // Validade input
+  if (title === "" || body === "") {
+    ui.showAlert("Please fill in all fields", "alert alert-danger");
+  } else {
+    // Check for ID to confirm if it is an add or submit data
+    if (id === "") {
+      // Create post
+      http
+        .post("http://localhost:3000/posts", data)
+        .then((data) => {
+          ui.showAlert("Post added", "alert alert-success");
+          ui.clearFields();
+          getPosts();
         })
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
+    } else {
+        // Update the post
+        http
+        .put(`http://localhost:3000/posts/${id}`, data)
+        .then((data) => {
+          ui.showAlert("Post updated", "alert alert-success");
+          ui.changeFormState('add');
+          getPosts();
+        })
+        .catch((err) => console.log(err));
     }
-
+  }
 }
 
 // Delete post
 
 function deletePost(e) {
-   
-    if(e.target.parentElement.classList.contains('delete')){
-        const id = e.target.parentElement.dataset.id;
-        console.log(e.target.parentElement.dataset)
-        if(confirm('Please, confirm you want to delete this post')){
-            http.delete(`http://localhost:3000/posts/${id}`)
-            .then(data => {
-                ui.showAlert('Post Removed', 'alert alert-success');
-                getPosts();
-            })
-            .catch(err => console.log(err));
-        }
+  if (e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+    console.log(e.target.parentElement.dataset);
+    if (confirm("Please, confirm you want to delete this post")) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then((data) => {
+          ui.showAlert("Post Removed", "alert alert-success");
+          getPosts();
+        })
+        .catch((err) => console.log(err));
     }
-    e.preventDefault();
+  }
+  e.preventDefault();
 }
 
 // Enable Edit state
 
-function enableEdit(e){
+function enableEdit(e) {
+  if (e.target.parentElement.classList.contains("edit")) {
+    const id = e.target.parentElement.dataset.id;
+    const title =
+      e.target.parentElement.previousElementSibling.previousElementSibling
+        .textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
 
-    if(e.target.parentElement.classList.contains('edit')){
-        const id = e.target.parentElement.dataset.id;
-        const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
-        const body = e.target.parentElement.previousElementSibling.textContent;
-        
-        const data = {
-            id,
-            title,
-            body
-        }
+    const data = {
+      id,
+      title,
+      body,
+    };
 
-        // Fill form with current post
-        ui.fillForm(data);
-    }
+    // Fill form with current post
+    ui.fillForm(data);
+  }
 
-    e.preventDefault();
+  e.preventDefault();
 }
 
 // Cancel edit state
 
-function cancelEdit(e){
-    if(e.target.classList.contains('post-cancel')){
-        ui.changeFormState('add');
-    }
+function cancelEdit(e) {
+  if (e.target.classList.contains("post-cancel")) {
+    ui.changeFormState("add");
+  }
 
-    e.preventDefault();
+  e.preventDefault();
 }
