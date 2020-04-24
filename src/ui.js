56ddc24acb769d9661e18f10476ca73e798a06ee
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-24 12:40:19 +1200

    create a ui method that change the state of the form by checking if the state is edit or not, if the state is edit creates a button to update the state and also offer another option with the option to cancel the edit and if the state is not edit, return the button submit to the Post it state, removes the cancel button option and also clean the id input field

--- src/ui.js
+++ src/ui.js
@@ -44,14 +44,13 @@ class UI {
         div.appendChild(document.createTextNode(message));
         // Get parent
         const container = document.querySelector('.postsContainer');
-        // Get posts
+        // Get element posts to insert before
         const posts = document.querySelector('#posts');
         // Insert alert div
         container.insertBefore(div, posts);
 
         // Timeout
-        setTimeout(() => {
-            this.clearAlert();
+        setTimeout(() => {primary
         }, 2000);
     }
 
@@ -75,6 +74,40 @@ class UI {
         this.titleInput.value = data.title;
         this.bodyInput.value = data.body;
         this.idInput.value = data.id;
+
+        this.changeFormState('edit');
+    }
+
+    // Change the form state
+    changeFormState(type){
+        if(type === 'edit'){
+            this.postSubmit.textContent = 'Update Post';
+            this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+            // Create cancel button
+            const button = document.createElement('button');
+            button.className = 'post-cancel btn btn-light btn-block';
+            button.appendChild(document.createTextNode('Cancel Edit'));
+
+            // Get parent
+            const cardForm = document.querySelector('.card-form');
+            // Get element to insert before
+            const formEnd = document.querySelector('.form-end');
+            // Insert cancel button
+            cardForm.insertBefore(button, formEnd);
+
+        } else {
+            this.postSubmit.textContent = 'Post it';
+            this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+            // Remove cancel btn if it is there
+            if(document.querySelector('.post-cancel')){
+                document.querySelector('.post-cancel').remove();
+            }
+            // clear ID from hidden filed
+            this.clearIdInput();
+            // clear text input fields
+            this.clearFields();
+        }
     }
 
 }
